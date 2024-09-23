8d8e5416f73b1c6ef08cb4c87efce6fe7c09c44e
IT24101397 <it24101397@my.sliit.lk>
2024-09-23 19:55:07 +0530

    Add files via upload

---
+++ Q2/IT24101397Lab07Q2A.java
@@ -0,0 +1,14 @@
+public class IT24101397Lab07Q2A {
+public static void main(String[] args) {
+       
+        
+        for(int i=0; i<4; i++){
+         for(int j=0; j<5; j++){
+        
+		System.out.print("$ ");
+        }
+         System.out.println("\n");
+        }
+}
+
+}
